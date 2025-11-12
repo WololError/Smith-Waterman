bd0ab50ace9485a4499a8cb95cfbed64583ac8eb
Sajid <abdellaouisajid@ulb.be>
2025-11-13 00:27:45 +0100

    Vector Prot

--- projetprelim.cpp
+++ projetprelim.cpp
@@ -13,24 +13,23 @@ int main(int argc, char** argv){
     string psqfile = string(argv[2]) + ".psq";
     string phrfile = string(argv[2]) + ".phr";
 
-    ifstream phr(phrfile, ios::binary);
-    ifstream psq(psqfile, ios::binary);
     Prot query = getIdandsequence(fastafile);
     dataPin pindata = read_pin(pinfile);
-    //cout << pindata.numberOfprot << endl;
-     string seq;
-     //auto start = std::chrono::high_resolution_clock::now();
-     for(int i = 0; i < pindata.sequence_offsets.size() - 1 ; i++){
-
-         seq = read_sequence(psq, pindata.sequence_offsets[i], pindata.sequence_offsets[i + 1]);
-         if (seq == query.sequence){
-             //cout << "protein Trouvee !" << " i = " << i << endl;
-             cout << read_header(phr,pindata.header_offsets[i],pindata.header_offsets[i + 1]) << endl;
-             //auto end = std::chrono::high_resolution_clock::now();
-             //std::chrono::duration<double> elapsed = end - start;
-             //std::cout << "Temps ecoule : " << elapsed.count() << " secondes\n";
-             break;
-       }
+    auto start = std::chrono::high_resolution_clock::now();    
+    vector<Protein> proteins = Protein::initProtlist(phrfile, psqfile, pindata); 
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "vector crée en : " << elapsed.count() << " secondes\n";
+    auto startp = std::chrono::high_resolution_clock::now(); 
+    for(int i = 0; i < proteins.size(); i++){
+        //cout << i << endl;
+        if (query.sequence == proteins[i].getseq()){
+            cout << proteins[i].getid() << endl;
+            auto endp = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> elapsed = endp - startp;
+            std::cout << "protein trouvé en : " << elapsed.count() << " secondes\n";
+            break;
+        }
     }
     return 0;
 }

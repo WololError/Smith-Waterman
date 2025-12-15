464101a7ea77798520cb1ad320b853104f16b3b0
Sajid <abdellaouisajid@ulb.be>
2025-12-15 20:16:14 +0100

    projet.cpp, projetopt.cpp et projetprelim signé

--- projet.cpp
+++ projet.cpp
@@ -18,7 +18,6 @@
 #include "headers/blast.h"
 #include "headers/blosum.h"
 #include "headers/Protein.h"
-#include "headers/SmithWaterman.h"
 
 int main(int argc, char** argv){
     string fastafile = argv[1];
@@ -38,7 +37,7 @@ int main(int argc, char** argv){
     
     Blosum scoring(blosumfile);
 
-    priority_queue<Protein> best20Prot = Protein::initProtqueueMT(phrfile, psqfile, pindata, query,scoring,GEP,GOP);
+    priority_queue<Protein> best20Prot = Protein::initProtqueue(phrfile, psqfile, pindata, query,scoring,GEP,GOP);
 
     Protein::print20best(best20Prot);
     
